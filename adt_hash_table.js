cb0f1b336c71ae629c0b462653f8763f39e2061e
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-04-08 19:56:11 -0700

    minor space fix

--- adt_hash_table.js
+++ adt_hash_table.js
@@ -50,6 +50,7 @@ function showDistro() {
 
 var someNames = ["David", "Jennifer", "Donnie", "Raymond", "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
 var hTable = new HashTable();
+
 for (var i = 0; i < someNames.length; ++i) {
   hTable.put(someNames[i]);
 }
