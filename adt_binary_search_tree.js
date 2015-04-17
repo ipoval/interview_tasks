24ab5615d80880adf18438c90785cc84262bc9da
Ivan Povalyukhin <ipovalyukhin@yammer-inc.com>
2015-04-16 22:54:00 -0700

    added inorder, preorder, postorder traversal for BST

--- adt_binary_search_tree.js
+++ adt_binary_search_tree.js
@@ -43,12 +43,69 @@ function insert(data) {
   }
 }
 
+BST.prototype = {
+  traverseInOrder: function() {
+    var parent = this.root;
+
+    function traverse(node) {
+      if (node !== null) {
+        traverse(node.left);
+        console.log(node.data);
+        traverse(node.right);
+      }
+    };
+
+    traverse(parent);
+  },
+
+  traversePreOrder: function() {
+    var parent = this.root;
+
+    function traverse(node) {
+      if (node !== null) {
+        console.log(node.data);
+        traverse(node.left);
+        traverse(node.right);
+      }
+    };
+
+    traverse(parent);
+  },
+
+  traversePostOrder: function() {
+    var parent = this.root;
+
+    function traverse(node) {
+      if (node !== null) {
+        traverse(node.left);
+        traverse(node.right);
+        console.log(node.data);
+      }
+    };
+
+    traverse(parent);
+  }
+}
+
 /*
  * __END__
  */
 
 var bst = new BST();
-bst.insert(30);
-bst.insert(20);
-bst.insert(50);
-bst.insert(60);
+bst.insert(23);
+bst.insert(45);
+bst.insert(16);
+bst.insert(37);
+bst.insert(3);
+bst.insert(99);
+bst.insert(22);
+
+bst.traverseInOrder();
+
+console.log(Array(30).join('-'));
+
+bst.traversePreOrder();
+
+console.log(Array(30).join('-'));
+
+bst.traversePostOrder();
