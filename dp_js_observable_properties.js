522a618aec3de2181c351acd9366d9718e88d36f
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-07-03 16:03:39 -0700

    experiment with simple observer pattern in javascript

---
+++ dp_js_observable_properties.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+/*
+ * Observable properties DP
+ */
+
+var Observable = function() {
+  var observersChanging = [], // property change handlers
+      observersChanged = [],
+      property = '';
+
+  Object.defineProperty(this, 'property', {
+    get: function() {
+      return property;
+    },
+    set: function(val) {
+      if (val !== undefined && val !== property) {
+
+        // Invoke property changing handlers
+        for (var i = 0; i < observersChanging.length; ++i) {
+          var observer = observersChanging[i];
+          if (!observer(val)) {
+            console.error('cancelled by observer validation');
+            return val;
+          }
+        }
+
+        property = val;
+
+        // Invoke property changed handlers
+        for (var i = 0; i < observersChanged.length; ++i) {
+          var observer = observersChanged[i];
+          observer(this);
+        }
+      }
+      return val;
+    }
+  });
+
+  this.onPropertyChanging = function(callback) {
+    observersChanging.push(callback);
+  };
+
+  this.onPropertyChanged = function(callback) {
+    observersChanged.push(callback);
+  };
+}
+
+var observable = new Observable();
+
+observable.onPropertyChanging(function(value) {
+  if (value === 'bad') {
+    console.error('validation error');
+    return false;
+  }
+  return true;
+});
+
+observable.onPropertyChanged(function(observable) {
+  console.info('changed', observable.property);
+});
+
+observable.property = 'good';
+observable.property = 'bad';
